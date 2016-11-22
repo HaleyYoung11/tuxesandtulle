1a5f164b63aa3d28c128b583b54a3f43ad68eaf0
Haley Young <haleyeyoung11@gmail.com>
2016-11-22 14:29:56 -0600

    Changed background of home page to white

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
         <div class="header-image">
         </div>
+        <div class="white">
         <section class="narrow">
             <h2>Welcome</h2>
                <p>Here at Tuxes & Tulle we’re committed to meeting the needs of any event by offering a wide range of services.</p>
@@ -10,5 +11,6 @@
                <p class="center"><a href="contact.php" class="callout">Book your free consultation now!</a></p>
                <p>If you're a bride or groom looking for a little bit of help on your wedding day, let us know! We'd be happy to get to know you and see how our services can fit your needs.</p>
         </section>
+        </div>
 
 <?php include('footer.php');?>
